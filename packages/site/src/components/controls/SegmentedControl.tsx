8bb697fd7b9c8b1f4723f7ee8c574f0802791b1c
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2021-12-31 10:06:26 +0100

    Fix SegmentedControl unwanted layout animations

--- packages/site/src/components/controls/SegmentedControl.tsx
+++ packages/site/src/components/controls/SegmentedControl.tsx
@@ -7,7 +7,7 @@ import {
 import clsx from "clsx"
 import { LayoutGroup, motion } from "framer-motion"
 import { ReactChild, forwardRef } from "react"
-import { springy } from "../../transitions"
+import { instant, springy } from "../../transitions"
 
 export interface SegmentedControlProps
   extends Omit<ToggleGroupSingleProps, "type"> {
@@ -40,45 +40,48 @@ export const SegmentedControl = forwardRef<
 
   return (
     <LayoutGroup id={layoutId}>
-      <Root
-        className={clsx(
-          className,
-          "grid grid-flow-col auto-cols-fr gap-x-[4px] h-9 text-zinc-500 bg-zinc-100 dark:hover:bg-zinc-700 rounded-lg transition-colors hover:bg-zinc-150 dark:bg-zinc-750 dark:text-zinc-350"
-        )}
-        id={id}
-        {...props}
-        ref={ref}
-        type="single"
-      >
-        {options.map((option, index, options) => {
-          const isActive = option === value
-          const isAfterActive = options[index - 1] === value
+      <Root asChild ref={ref} {...props} type="single">
+        <motion.div
+          className={clsx(
+            className,
+            "grid grid-flow-col auto-cols-fr gap-x-[4px] h-9 text-zinc-500 bg-zinc-100 dark:hover:bg-zinc-700 rounded-lg transition-colors hover:bg-zinc-150 dark:bg-zinc-750 dark:text-zinc-350"
+          )}
+          id={id}
+          layoutId="root"
+          transition={instant}
+        >
+          {options.map((option, index, options) => {
+            const isActive = option === value
+            const isAfterActive = options[index - 1] === value
 
-          return (
-            <Item
-              className={clsx(
-                "flex first-of-type:before:hidden relative before:absolute before:left-[-3px] justify-center items-center px-3 before:w-[2px] before:h-1/2 text-sm font-medium before:bg-current rounded-lg before:rounded-full before:opacity-20 transition before:transition-opacity focusable",
-                {
-                  "text-primary-500 dark:text-primary-400": isActive,
-                  "before:opacity-0": isActive || isAfterActive
-                }
-              )}
-              key={index}
-              title={option}
-              value={option}
-            >
-              <span className="relative z-20">{labels[index] ?? option}</span>
-              {isActive && (
-                <motion.span
-                  aria-hidden
-                  className="absolute inset-0.5 z-10 bg-white dark:bg-zinc-600 rounded-md shadow"
-                  layoutId="background"
-                  transition={springy}
-                />
-              )}
-            </Item>
-          )
-        })}
+            return (
+              <Item asChild key={index} value={option}>
+                <motion.button
+                  className={clsx(
+                    "flex first-of-type:before:hidden relative before:absolute before:left-[-3px] justify-center items-center px-3 before:w-[2px] before:h-1/2 text-sm font-medium before:bg-current rounded-lg before:rounded-full before:opacity-20 transition before:transition-opacity focusable",
+                    {
+                      "text-primary-500 dark:text-primary-400": isActive,
+                      "before:opacity-0": isActive || isAfterActive
+                    }
+                  )}
+                  title={option}
+                >
+                  <span className="relative z-20">
+                    {labels[index] ?? option}
+                  </span>
+                  {isActive && (
+                    <motion.span
+                      aria-hidden
+                      className="absolute inset-0.5 z-10 bg-white dark:bg-zinc-600 rounded-md shadow"
+                      layoutId="background"
+                      transition={springy}
+                    />
+                  )}
+                </motion.button>
+              </Item>
+            )
+          })}
+        </motion.div>
       </Root>
     </LayoutGroup>
   )
