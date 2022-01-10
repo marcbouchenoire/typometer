5067fed5bef2753f9111d083217d8aca4393adc8
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-01-10 18:51:01 +0100

    Remove superfluous container

--- packages/site/src/components/sections/Editor.tsx
+++ packages/site/src/components/sections/Editor.tsx
@@ -260,161 +260,150 @@ export function Editor(props: ComponentProps<"section">) {
 
   return (
     <section {...props}>
-      <div className="flex flex-col rounded-md border dark:border-zinc-800 border-zinc-150">
-        <div className="flex flex-col sm:flex-row">
-          <div className="flex flex-col flex-1 min-w-0">
-            <div className="relative h-12 border-b dark:border-zinc-800 border-zinc-150">
-              <input
-                aria-label="Input (Press / to focus)"
-                className="px-4 pr-12 w-full h-full text-sm font-medium text-zinc-700 placeholder:text-zinc-700/50 dark:text-zinc-100 dark:placeholder:text-zinc-100/50 truncate bg-transparent rounded-t-md transition appearance-none sm:rounded-tr-none focusable"
-                onChange={handleValueChange}
-                placeholder="Type anything to measure it…"
-                ref={inputRef}
-                value={value}
-              />
-              <kbd className="flex absolute top-3.5 right-3.5 justify-center items-center w-5 h-5 text-xs font-medium leading-none text-zinc-400 bg-zinc-500/10 dark:bg-zinc-300/10 rounded-[0.2rem] pointer-events-none select-none dark:text-zinc-450">
-                /
-              </kbd>
-            </div>
-            <div className="relative flex-1 min-w-0">
-              <button
-                className="flex absolute right-5 bottom-5 gap-1.5 justify-center items-center px-2.5 pr-2 h-9 text-sm font-medium rounded-md backdrop-blur-md transition cursor-pointer sm:right-6 sm:bottom-6 hover:bg-primary-500/20 dark:hover:bg-primary-400/30 focusable text-primary-500 bg-primary-500/10 dark:bg-primary-400/20 dark:text-primary-400"
-                onClick={handleMeasureClick}
+      <div className="flex flex-col first-letter:rounded-md border dark:border-zinc-800 sm:flex-row border-zinc-150">
+        <div className="flex flex-col flex-1 min-w-0">
+          <div className="relative h-12 border-b dark:border-zinc-800 border-zinc-150">
+            <input
+              aria-label="Input (Press / to focus)"
+              className="px-4 pr-12 w-full h-full text-sm font-medium text-zinc-700 placeholder:text-zinc-700/50 dark:text-zinc-100 dark:placeholder:text-zinc-100/50 truncate bg-transparent rounded-t-md transition appearance-none sm:rounded-tr-none focusable"
+              onChange={handleValueChange}
+              placeholder="Type anything to measure it…"
+              ref={inputRef}
+              value={value}
+            />
+            <kbd className="flex absolute top-3.5 right-3.5 justify-center items-center w-5 h-5 text-xs font-medium leading-none text-zinc-400 bg-zinc-500/10 dark:bg-zinc-300/10 rounded-[0.2rem] pointer-events-none select-none dark:text-zinc-450">
+              /
+            </kbd>
+          </div>
+          <div className="relative flex-1 min-w-0">
+            <button
+              className="flex absolute right-5 bottom-5 gap-1.5 justify-center items-center px-2.5 pr-2 h-9 text-sm font-medium rounded-md backdrop-blur-md transition cursor-pointer sm:right-6 sm:bottom-6 hover:bg-primary-500/20 dark:hover:bg-primary-400/30 focusable text-primary-500 bg-primary-500/10 dark:bg-primary-400/20 dark:text-primary-400"
+              onClick={handleMeasureClick}
+            >
+              <span className="tabular-nums">
+                {duration}
+                <span className="text-primary-500/60 dark:text-primary-400/50">
+                  ms
+                </span>
+              </span>
+              <motion.svg
+                animate={{ rotate: key * 360 }}
+                className="flex-none opacity-30 dark:opacity-50"
+                height="20"
+                role="presentation"
+                transition={arrowTransition}
+                width="20"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <span className="tabular-nums">
-                  {duration}
-                  <span className="text-primary-500/60 dark:text-primary-400/50">
-                    ms
-                  </span>
-                </span>
-                <motion.svg
-                  animate={{ rotate: key * 360 }}
-                  className="flex-none opacity-30 dark:opacity-50"
-                  height="20"
-                  role="presentation"
-                  transition={arrowTransition}
-                  width="20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5 10a5 5 0 0 1 9-3h-2a1 1 0 1 0 0 2h4a1 1 0 0 0 1-1V4a1 1 0 1 0-2 0v1.101a7 7 0 1 0 1.063 8.4 1 1 0 1 0-1.731-1.002A5 5 0 0 1 5 10Z"
-                    fill="currentColor"
-                  />
-                </motion.svg>
-              </button>
-              <Metrics className="flex-1 p-5 h-full sm:p-6" metrics={metrics} />
+                <path
+                  d="M5 10a5 5 0 0 1 9-3h-2a1 1 0 1 0 0 2h4a1 1 0 0 0 1-1V4a1 1 0 1 0-2 0v1.101a7 7 0 1 0 1.063 8.4 1 1 0 1 0-1.731-1.002A5 5 0 0 1 5 10Z"
+                  fill="currentColor"
+                />
+              </motion.svg>
+            </button>
+            <Metrics className="flex-1 p-5 h-full sm:p-6" metrics={metrics} />
+          </div>
+        </div>
+        <div className="flex flex-col flex-none gap-5 p-5 w-full border-t dark:border-zinc-800 sm:p-6 sm:w-64 sm:border-t-0 sm:border-l md:w-80 border-zinc-150">
+          <Label className="flex flex-col cursor-pointer" htmlFor="font-size">
+            <div className="flex items-center mb-2">
+              <span className="text-sm font-medium text-zinc-800 dark:text-zinc-100">
+                Size
+              </span>
+              <span className="ml-auto text-2xs">
+                <code className="text-zinc-600 dark:text-zinc-300">
+                  {size}px
+                </code>
+              </span>
             </div>
-          </div>
-          <div className="flex flex-col flex-none gap-5 p-5 w-full border-t dark:border-zinc-800 sm:p-6 sm:w-64 sm:border-t-0 sm:border-l md:w-80 border-zinc-150">
-            <Label className="flex flex-col cursor-pointer" htmlFor="font-size">
-              <div className="flex items-center mb-2">
-                <span className="text-sm font-medium text-zinc-800 dark:text-zinc-100">
-                  Size
+            <Slider
+              id="font-size"
+              max={64}
+              min={8}
+              onValueChange={handleSizeChange}
+              step={1}
+              value={size}
+            />
+          </Label>
+          <Label className="flex flex-col cursor-pointer" htmlFor="font-weight">
+            <div className="flex items-center mb-2">
+              <span className="text-sm font-medium text-zinc-800 dark:text-zinc-100">
+                Weight
+              </span>
+              <span className="ml-auto text-2xs">
+                <code className="text-zinc-600 dark:text-zinc-300">
+                  {weight}
+                </code>
+              </span>
+            </div>
+            <Select
+              className="w-full"
+              selectProps={{
+                id: "font-weight",
+                children: weightOptions,
+                onChange: handleWeightChange,
+                value: weight
+              }}
+            />
+          </Label>
+          <Label className="flex flex-col cursor-pointer" htmlFor="font-family">
+            <div className="flex items-center mb-2">
+              <span className="text-sm font-medium text-zinc-800 dark:text-zinc-100">
+                Family
+              </span>
+              <span className="ml-auto text-2xs">
+                <code className="text-zinc-600 dark:text-zinc-300">
+                  &quot;{family}&quot;
+                </code>
+              </span>
+            </div>
+            <SegmentedControl
+              className="w-full"
+              id="font-family"
+              labels={[
+                <span className="font-sans" key="sans-serif">
+                  Aa
+                </span>,
+                <span className="font-serif" key="serif">
+                  Aa
+                </span>,
+                <span className="font-mono" key="monospace">
+                  Aa
+                </span>,
+                <span className="font-cursive" key="cursive">
+                  Aa
                 </span>
-                <span className="ml-auto text-2xs">
-                  <code className="text-zinc-600 dark:text-zinc-300">
-                    {size}px
-                  </code>
+              ]}
+              onValueChange={handleFamilyChange}
+              options={["sans-serif", "serif", "monospace", "cursive"]}
+              value={family}
+            />
+          </Label>
+          <Label className="flex flex-col cursor-pointer" htmlFor="font-style">
+            <div className="flex items-center mb-2">
+              <span className="text-sm font-medium text-zinc-800 dark:text-zinc-100">
+                Style
+              </span>
+              <span className="ml-auto text-2xs">
+                <code className="text-zinc-600 dark:text-zinc-300">
+                  &quot;{style}&quot;
+                </code>
+              </span>
+            </div>
+            <SegmentedControl
+              className="w-full"
+              id="font-style"
+              labels={[
+                <span key="normal">Normal</span>,
+                <span className="italic" key="italic">
+                  Italic
                 </span>
-              </div>
-              <Slider
-                id="font-size"
-                max={64}
-                min={8}
-                onValueChange={handleSizeChange}
-                step={1}
-                value={size}
-              />
-            </Label>
-            <Label
-              className="flex flex-col cursor-pointer"
-              htmlFor="font-weight"
-            >
-              <div className="flex items-center mb-2">
-                <span className="text-sm font-medium text-zinc-800 dark:text-zinc-100">
-                  Weight
-                </span>
-                <span className="ml-auto text-2xs">
-                  <code className="text-zinc-600 dark:text-zinc-300">
-                    {weight}
-                  </code>
-                </span>
-              </div>
-              <Select
-                className="w-full"
-                selectProps={{
-                  id: "font-weight",
-                  children: weightOptions,
-                  onChange: handleWeightChange,
-                  value: weight
-                }}
-              />
-            </Label>
-            <Label
-              className="flex flex-col cursor-pointer"
-              htmlFor="font-family"
-            >
-              <div className="flex items-center mb-2">
-                <span className="text-sm font-medium text-zinc-800 dark:text-zinc-100">
-                  Family
-                </span>
-                <span className="ml-auto text-2xs">
-                  <code className="text-zinc-600 dark:text-zinc-300">
-                    &quot;{family}&quot;
-                  </code>
-                </span>
-              </div>
-              <SegmentedControl
-                className="w-full"
-                id="font-family"
-                labels={[
-                  <span className="font-sans" key="sans-serif">
-                    Aa
-                  </span>,
-                  <span className="font-serif" key="serif">
-                    Aa
-                  </span>,
-                  <span className="font-mono" key="monospace">
-                    Aa
-                  </span>,
-                  <span className="font-cursive" key="cursive">
-                    Aa
-                  </span>
-                ]}
-                onValueChange={handleFamilyChange}
-                options={["sans-serif", "serif", "monospace", "cursive"]}
-                value={family}
-              />
-            </Label>
-            <Label
-              className="flex flex-col cursor-pointer"
-              htmlFor="font-style"
-            >
-              <div className="flex items-center mb-2">
-                <span className="text-sm font-medium text-zinc-800 dark:text-zinc-100">
-                  Style
-                </span>
-                <span className="ml-auto text-2xs">
-                  <code className="text-zinc-600 dark:text-zinc-300">
-                    &quot;{style}&quot;
-                  </code>
-                </span>
-              </div>
-              <SegmentedControl
-                className="w-full"
-                id="font-style"
-                labels={[
-                  <span key="normal">Normal</span>,
-                  <span className="italic" key="italic">
-                    Italic
-                  </span>
-                ]}
-                onValueChange={handleStyleChange}
-                options={["normal", "italic"]}
-                value={style}
-              />
-            </Label>
-          </div>
+              ]}
+              onValueChange={handleStyleChange}
+              options={["normal", "italic"]}
+              value={style}
+            />
+          </Label>
         </div>
       </div>
     </section>
