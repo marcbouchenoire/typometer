c8817e441163d9d3ece796f01cff1ea3e05e8bf4
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2022-04-12 23:23:35 +0200

    Fix ambient types

--- packages/site/declarations.d.ts
+++ packages/site/declarations.d.ts
@@ -1,17 +1,5 @@
-import { ReactNode } from "react"
-
 declare module "*/data.json" {
   const value: any
 
   export default value
-}
-
-// TODO: Remove when next-themes fixes this @types/react^18.0.0-related change
-declare module "next-themes" {
-  export interface ThemeProviderProps {
-    /**
-     * A set of children.
-     */
-    children?: ReactNode
-  }
 }
