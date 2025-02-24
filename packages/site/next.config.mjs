75dc21395deeafe3db84d46ad9a9d9d45eed606d
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2025-02-24 13:37:02 +0100

    Move back to subdomain

--- packages/site/next.config.mjs
+++ packages/site/next.config.mjs
@@ -27,8 +27,6 @@ export default () => {
   storeStaticFiles()
 
   return {
-    assetPrefix:
-      process.env.NODE_ENV === "production" ? "/projects/typometer" : undefined,
     trailingSlash: false,
     eslint: {
       ignoreDuringBuilds: true
